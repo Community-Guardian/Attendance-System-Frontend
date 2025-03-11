b521786b75b425c9d2b2af6fc9ebfba5c2b8ee17
Nrad8394 <94044917+Nrad8394@users.noreply.github.com>
2025-03-11 14:00:08 +0300

    update

---
+++ class-attendance/handler/customApiConfig.tsx
@@ -0,0 +1,39 @@
+
+
+// Attendance URLs
+export const ATTENDANCE_SESSION_URL = `/attendance/attendance_sessions/`;
+export const ATTENDANCE_RECORD_URL = `/attendance/attendance_records/`;
+
+// Borrow Accounts URLs
+export const BORROW_ACCOUNT_URL = `/borrow/borrow-accounts/`;
+export const FACIAL_RECOGNITION_DATA_URL = `/borrow/facial-recognition/`;
+export const BORROWED_ATTENDANCE_RECORD_URL = `/borrow/borrowed-attendance/`;
+
+// Config URLs
+export const SYSTEM_SETTINGS_URL = `/system-settings//`;
+export const CAT_ATTENDANCE_WEEK_URL = `/cat-weeks/`;
+export const TIMETABLE_RULE_URL = `/timetable-rules/`;
+export const AUDIT_LOG_URL = `/audit-logs/`;
+
+// Courses URLs
+export const DEPARTMENT_URL = `/departments/`;
+export const COURSE_URL = `/courses/`;
+
+// Geolocation URLs
+export const GEOLOCATION_ZONE_URL = `/geolocation/geolocation_zones/`;
+
+// Reports URLs
+export const ATTENDANCE_REPORT_URL = `/attendance-reports/`;
+export const STUDENT_ATTENDANCE_HISTORY_URL = `/student-history/`;
+export const TIMETABLE_ADHERENCE_URL = `/timetable-adherence/`;
+
+// Timetables URLs
+export const TIMETABLE_URL = `/timetables/`;
+export const TIMETABLE_PDF_URL = `/timetables/timetable-pdfs/`;
+
+// UserManager URLs
+export const CUSTOM_USER_URL = `/api/users/`;
+export const MAC_ADDRESS_UPDATE_URL = `/api/update-mac/`;
+export const PASSWORD_RESET_URL = `/user/password/reset/`;
+export const PASSWORD_RESET_CONFIRM_URL = `/reset/`;
+export const TOKEN_REFRESH_URL = `/token/refresh/`;
